5ea15841d78c1b53e656d1f12c86c2826d6ff5a4
jmartincampos <jmartincampos18@gmail.com>
2024-04-25 10:50:13 -0700

    source code

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const User = require('./user');
+const Product = require('./product');
+const Category = require('./category');
+const Order = require('./Order');
+
+module.exports = { User, Product, Category, Order };
