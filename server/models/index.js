4ce36846c04beb1f525cbeef0f809cbb7d894054
jmartincampos <jmartincampos18@gmail.com>
2024-04-25 11:51:42 -0700

    index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
 const Product = require('./product');
 const Category = require('./category');
-const Order = require('./Order');
+const Order = require('./order');
 
 module.exports = { User, Product, Category, Order };
