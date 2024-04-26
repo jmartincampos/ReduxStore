4edc9281588aa1adb060dff071c8cb549ebef137
jmartincampos <jmartincampos18@gmail.com>
2024-04-25 17:01:39 -0700

    jsx updates

--- Redux/client/src/main.jsx
+++ Redux/client/src/main.jsx
@@ -3,8 +3,48 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Home from './pages/home';
+import Detail from './pages/detail';
+import NoMatch from './pages/noMatch';
+import Login from './pages/login';
+import Signup from './pages/signUp';
+import Success from './pages/success';
+import OrderHistory from './pages/orderHistory';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    error: <NoMatch />,
+    children: [
+      {
+        index: true, 
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/success',
+        element: <Success />
+      }, {
+        path: '/orderHistory',
+        element: <OrderHistory />
+      }, {
+        path: '/products/:id',
+        element: <Detail />
+      }
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
